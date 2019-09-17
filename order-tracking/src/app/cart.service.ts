9a78989b8cf672ddf830f9c5155c666bcf845bf2
adrian609 <Rodriguez.Adrian609@gmail.com>
2019-09-16 20:14:33 -0400

    Manage data implmented and tested

--- order-tracking/src/app/cart.service.ts
+++ order-tracking/src/app/cart.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
 
-  constructor() { }
+export class CartService {
+  items = [];
+
+  constructor(
+    private cartService: CartService,
+    private http: HttpClient
+  ) { }
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
 }
